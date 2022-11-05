1190a19acffd17cac8c2a12504674e0f2d14dbb8
Rahmat <oktrifianto@gmail.com>
2022-11-05 20:21:25 +0700

    add prop in Button component

--- src/Components/Button/AddButton.js
+++ src/Components/Button/AddButton.js
@@ -1,8 +1,9 @@
 import { ReactComponent as AddIcon } from '../../Assets/Icons/plus.svg';
 
-export default function AddButton(){
+export default function AddButton({isClickButton}){
   return (
-    <button 
+    <button
+      onClick={isClickButton}
       type="button"
       className="text-white bg-[#16abf8] text-lg font-semibold rounded-full px-5 py-2.5 mr-2 mb-2 min-w-[170px] h-[54px]">
         <span><AddIcon style={{display: "inline-block"}} /></span> {/* switch to loading button when call API */}
