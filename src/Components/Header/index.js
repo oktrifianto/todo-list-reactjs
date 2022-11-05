6d1ce09f6d0639467cd62b7444e10032f6d1d4bd
Rahmat <oktrifianto@gmail.com>
2022-11-05 11:48:38 +0700

    init `header` component

---
+++ src/Components/Header/index.js
@@ -0,0 +1,9 @@
+export default function Header(){
+  return (
+    <div className="bg-[#16abf8]">
+      <div className="container max-w-5xl my-0 mx-auto flex items-center" data-cy="header-background">
+        <h2 className="text-2xl text-white font-bold text-left h-[105px] py-8">TO DO LIST APP</h2>
+      </div>
+    </div>
+  );
+}
