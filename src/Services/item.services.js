01e12d64abe0065d880eb1fbfddeba1e3d4cdd74
Rahmat <oktrifianto@gmail.com>
2022-11-06 13:23:42 +0700

    create method `getDetailActivity`

--- src/Services/item.services.js
+++ src/Services/item.services.js
@@ -1,5 +1,15 @@
 import axiosAuth from "./API/axios";
 
+export async function getDetailActivity(id_group){
+  try {
+    const response = await axiosAuth.get(`/activity-groups/${id_group}`);
+    return response.data;
+  } catch (error) {
+    return error.response;
+  }
+}
+
+// Used for update data
 export async function getListItem(id_group){
   try {
     const response = await axiosAuth.get(`/todo-items?activity_group_id=${id_group}`);
