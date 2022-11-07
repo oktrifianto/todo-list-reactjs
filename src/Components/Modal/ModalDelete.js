ff90f84e611ce7ac0b0d1da858712f6fedbac0dd
Rahmat <oktrifianto@gmail.com>
2022-11-07 07:43:14 +0700

    create new prop `hasCancel`

--- src/Components/Modal/ModalDelete.js
+++ src/Components/Modal/ModalDelete.js
@@ -2,7 +2,7 @@ import { ReactComponent as AlertIcon } from '../../Assets/Icons/alert.svg';
 import CancelButton from '../Button/CancelButton';
 import DeleteButton from '../Button/DeleteButton';
 
-export default function ModalDelete({hasDelete}){ // @see https://stackoverflow.com/a/66186184
+export default function ModalDelete({typeName, hasCancel, hasDelete}){ // @see https://stackoverflow.com/a/66186184
 
   return (
     <div tabIndex="-1" className="bg-black/50 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
@@ -16,10 +16,10 @@ export default function ModalDelete({hasDelete}){ // @see https://stackoverflow.
               <AlertIcon className="mx-auto mt-4 mb-4 w-20 h-20" />
             </div>
             <div className="modal-body">
-              <p className="mt-12 mb-12 text-lg font-normal text-gray-800">Apakah anda yakin menghapus activity <strong>"New Activity"</strong></p>
+              <p className="mt-12 mb-12 text-lg font-normal text-gray-800">Apakah anda yakin menghapus {typeName} <strong>"New Activity"</strong></p>
             </div>
             <div className="modal-footer">
-              <CancelButton />
+              <CancelButton hasCancel={hasCancel} />
               <DeleteButton hasDelete={hasDelete} />
             </div>
 
