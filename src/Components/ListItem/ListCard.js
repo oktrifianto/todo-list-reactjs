bc124b1a5dc6385ba8c2e7c7ccd3c6a175702f0d
Rahmat <oktrifianto@gmail.com>
2022-11-08 08:39:28 +0700

    fix: checked form and strikeline success

--- src/Components/ListItem/ListCard.js
+++ src/Components/ListItem/ListCard.js
@@ -6,18 +6,15 @@ import { updateStatusListItem } from '../../Services/item.services';
 
 export default function ListCard({id, title, priority, is_active, setDeleteItem, setDelItemID}){
   const [isChecked, setIsChecked] = useState(false);
-  const [isStreak, setIsStreak]   = useState((is_active) => is_active === 1 ? 1 : 0);
+  const [isStreak, setIsStreak]   = useState(is_active === 0 ? true : false);
 
   const handleChange = async (event) => {
     if (event.target.checked) {
-      // centang 0 ... ini done --- not active
       const result = await updateStatusListItem(id, 0);
-      // console.log(result);
       if (result.status === 200) {
         setIsStreak(true);
       }
     } else {
-      // uncentang .... active list
       const result = await updateStatusListItem(id, 1);
       if (result.status === 200) {
         setIsStreak(false);
@@ -28,18 +25,15 @@ export default function ListCard({id, title, priority, is_active, setDeleteItem,
 
   return (
     <div className="content-item flex justify-between p-7 mb-4 bg-white rounded-xl border border-gray-200 shadow-md">
-      {/*properi value */}
       <div className="flex items-center">
         <input
           value={isChecked} 
           onChange={handleChange}
-          type="checkbox" 
+          type="checkbox"
+          defaultChecked={is_active === 0 ? true : false}
           className="h-[24px] w-[24px] mr-4 rounded" />
         <Indicator priority={priority} />
-        { !isStreak ? 
-          <span className="mr-4 font-medium text-lg">{title}</span> :
-          <span className="mr-4 font-medium text-lg" style={{textDecorationLine: 'line-through', textDecorationStyle: 'solid'}}>{title}</span>
-        }
+        <span className={`mr-4 font-medium text-lg ${ isStreak ? 'line-through text-[#888]' : ''}`}>{title}</span>
         <EditIcon />
       </div>
 
@@ -47,7 +41,6 @@ export default function ListCard({id, title, priority, is_active, setDeleteItem,
       <button onClick={() => { setDeleteItem(true); setDelItemID(id) } }>
         <TrashIcon className="cursor-pointer" />
       </button>
-      {/* { isStreak && <p>{isStreak}</p> } */}
     </div>
   );
 }
