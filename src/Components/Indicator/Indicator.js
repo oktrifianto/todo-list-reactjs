84e13189119bc36050c3b16bc5f4c76735e806f4
Rahmat <oktrifianto@gmail.com>
2022-11-08 16:45:15 +0700

    fix: update indicator bug color

--- src/Components/Indicator/Indicator.js
+++ src/Components/Indicator/Indicator.js
@@ -1,14 +1,14 @@
 // const colors = ['#7a5195', '#bc5090','#ef5675'];
 
 export default function Indicator({priority}){
-  let color;
-  if (priority === 'very-high') color = '#ed4c5c';
-  if (priority === 'high') color = '#f8a541';
-  if (priority === 'normal') color = '#00a790';
-  if (priority === 'low') color = '#428bc1';
-  if (priority === 'very-low') color = '#8942c1';
+  // let color;
+  // if (priority === 'very-high') color = '#ed4c5c';
+  // if (priority === 'high') color = '#f8a541';
+  // if (priority === 'normal') color = '#00a790';
+  // if (priority === 'low') color = '#428bc1';
+  // if (priority === 'very-low') color = '#8942c1';
 
   return (
-    <span className={`inline-flex items-center p-[7px] mr-4 ml-3 text-sm font-semibold bg-[${color}] rounded-full`}></span>
+    <span className={`inline-flex items-center p-[7px] mr-4 ml-3 text-sm font-semibold bg-[${ priority === 'very-high' ? '#ed4c5c' : priority === 'high' ? '#f8a541' : priority === 'normal' ? '#00a790' : priority === 'low' ? '#428bc1' : priority === 'very-low' ? '#8942c1' : ''}] rounded-full`}></span>
   );
 }
