a4b4f2bc99f741a20884346b372d192e62cc338e
Rahmat <oktrifianto@gmail.com>
2022-11-08 11:57:02 +0700

    update SaveButton component

--- src/Components/Button/SaveButton.js
+++ src/Components/Button/SaveButton.js
@@ -1,6 +1,7 @@
-export default function SaveButton(){
+export default function SaveButton({hasClick}){
   return (
     <button
+      onClick={hasClick}
       type="button"
       className="text-white bg-[#16abf8] text-lg font-semibold rounded-full px-5 py-2.5 mr-2 mb-2 min-w-[170px] h-[54px]">
         Simpan
