2845dd42f554c4e73c81878ecdb1b1eafbf5c600
Rahmat <oktrifianto@gmail.com>
2022-11-08 12:30:51 +0700

    update some `data-cy` value

--- src/Components/Button/SaveButton.js
+++ src/Components/Button/SaveButton.js
@@ -1,8 +1,9 @@
-export default function SaveButton({hasClick}){
+export default function SaveButton({hasClick, datacy}){
   return (
     <button
       onClick={hasClick}
       type="button"
+      data-cy={datacy}
       className="text-white bg-[#16abf8] text-lg font-semibold rounded-full px-5 py-2.5 mr-2 mb-2 min-w-[170px] h-[54px]">
         Simpan
     </button>
--- src/Components/Modal/ModalAddList.js
+++ src/Components/Modal/ModalAddList.js
@@ -25,7 +25,7 @@ export default function ModalAddList({setLoading, setListItem, setAddItem, id_gr
 
   return (
     <div tabIndex="-1" className="bg-black/50 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
-      <div className="relative pt-20 w-full max-w-3xl h-full my-0 mx-auto md:h-auto">
+      <div className="relative pt-20 w-full max-w-3xl h-full my-0 mx-auto md:h-auto" data-cy="modal-add">
         <div className="relative bg-white rounded-lg shadow flex items-center" style={{margin: "1.75rem auto"}}>
           <div className="modal-content text-center w-full">
             <div className="modal-header flex justify-between p-5" style={{borderBottom: "1px solid #ccc"}}>
@@ -35,25 +35,34 @@ export default function ModalAddList({setLoading, setListItem, setAddItem, id_gr
             <div className="modal-body">
               <form className="p-8">
                 <div className="mb-6">
-                    <label className="text-left block mb-2 text-sm font-medium text-gray-900">NAMA LIST ITEM</label>
-                    <input type="text" name="title" onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Tambahkan Nama List" required />
+                    <label data-cy="modal-add-name-title" className="text-left block mb-2 text-sm font-medium text-gray-900">NAMA LIST ITEM</label>
+                    <input 
+                      type="text" 
+                      name="title" 
+                      onChange={handleChange}
+                      data-cy="modal-add-name-input"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Tambahkan Nama List" required />
                 </div>
                 <div className="mb-6">
-                  <label forhtml="default" className="block mb-2 text-sm font-medium text-gray-900 text-left">PRIORITY</label>
-                  <select onChange={handleChange} name="priority" className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                    <option value="select">Select Priority</option>
-                    <option value="very-high">Very High</option>
-                    <option value="high">High</option>
-                    <option value="normal">Normal</option>
-                    <option value="low">Low</option>
-                    <option value="very-low">Very Low</option>
+                  <label forhtml="default" data-cy="modal-add-priority-title" className="block mb-2 text-sm font-medium text-gray-900 text-left">PRIORITY</label>
+                  <select
+                    onChange={handleChange} 
+                    name="priority" 
+                    data-cy="modal-add-priority-dropdown"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                      <option value="select">Select Priority</option>
+                      <option value="very-high">Very High</option>
+                      <option value="high">High</option>
+                      <option value="normal">Normal</option>
+                      <option value="low">Low</option>
+                      <option value="very-low">Very Low</option>
                   </select>
                 </div>
 
               </form>
             </div>
             <div className="modal-footer p-4 flex flex-row-reverse" style={{borderTop: "1px solid #ccc"}}>
-              <SaveButton className="text-right" hasClick={() => createNewListItem()} />
+              <SaveButton datacy="modal-add-save-button" className="text-right" hasClick={() => createNewListItem()} />
             </div>
           </div>
         </div>
