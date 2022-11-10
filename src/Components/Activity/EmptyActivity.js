3275a806a5bb540dc9aee9e30330d7e7c9c41285
Rahmat <oktrifianto@gmail.com>
2022-11-10 11:47:47 +0700

    add `data-cy` on empty activity

--- src/Components/Activity/EmptyActivity.js
+++ src/Components/Activity/EmptyActivity.js
@@ -2,7 +2,7 @@ import EmptyActivityImage from '../../Assets/Images/activity.png';
 
 export default function EmptyActivity(){
   return (
-    <div className="dashboard-content flex flex-wrap justify-center">
+    <div className="dashboard-content flex flex-wrap justify-center" data-cy="activity-empty-state">
       <img className="justify-items-center" src={EmptyActivityImage} alt="activity" />
     </div>
   );
